8d35d1eedb814bd4173a3efa4c24038b89b024a4
dalilaham <dalila.2hamidouche@gmail.com>
2016-06-26 18:32:36 +0200

    Your commit message

---
+++ hi/Mainclass.java
@@ -0,0 +1,10 @@
+package hi;
+
+/**
+ * Created by user on 25/06/2016.
+ */
+public class Mainclass {
+public static void main( String [] args){
+    System.out.println("hello wolrd!\n"+"Dalila");
+}
+}
